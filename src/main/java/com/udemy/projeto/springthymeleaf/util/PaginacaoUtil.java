d92bc93a2c40d8d36531fa6ea7cc483d5323e3a1
Albertinesilva <20191tadssaj0026-ifba-edu-br>
2023-05-22 20:08:23 -0300

    Paginação Atualizada

--- src/main/java/com/udemy/projeto/springthymeleaf/util/PaginacaoUtil.java
+++ src/main/java/com/udemy/projeto/springthymeleaf/util/PaginacaoUtil.java
@@ -6,13 +6,14 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
+@Getter
 @NoArgsConstructor
 @AllArgsConstructor
-@Getter
 public class PaginacaoUtil<T> {
     
     private int tamanho;
     private int pagina;
     private long totalDePaginas;
+    private String direcao;
     private List<T> registros;
 }
--- src/main/java/com/udemy/projeto/springthymeleaf/web/controllers/CargoController.java
+++ src/main/java/com/udemy/projeto/springthymeleaf/web/controllers/CargoController.java
@@ -1,6 +1,7 @@
 package com.udemy.projeto.springthymeleaf.web.controllers;
 
 import java.util.List;
+import java.util.Optional;
 
 import javax.validation.Valid;
 
@@ -13,12 +14,14 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.udemy.projeto.springthymeleaf.entities.Cargo;
 import com.udemy.projeto.springthymeleaf.entities.Departamento;
 import com.udemy.projeto.springthymeleaf.service.CargoService;
 import com.udemy.projeto.springthymeleaf.service.DepartamentoService;
+import com.udemy.projeto.springthymeleaf.util.PaginacaoUtil;
 
 @Controller
 @RequestMapping("/cargos")
@@ -36,8 +39,15 @@ public class CargoController {
     }
 
     @GetMapping("/listar")
-    public String listar(ModelMap model) {
-        model.addAttribute("cargos", cargoService.buscarTodos());
+    public String listar(ModelMap model, @RequestParam("page") Optional<Integer> page,
+                                         @RequestParam("dir") Optional<String> dir) {
+
+        int paginaAtual = page.orElse(1);
+        String ordem = dir.orElse("asc");
+
+        PaginacaoUtil<Cargo> pageCargo = cargoService.buscaPorPagina(paginaAtual, ordem);
+
+        model.addAttribute("pageCargo", pageCargo);
         return "/cargo/lista";
     }
 
